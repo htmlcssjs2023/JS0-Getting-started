53e3ab768146b14c9e0235141d7a7aab42de5302
mdabdulhalim <htmlcssjs2023>
2023-07-17 10:43:33 +0600

    discuss comment & syntax

--- L0.js
+++ L0.js
@@ -8,3 +8,18 @@ let n1 = 10;
 let n2 = 20;
 let doTotal = n1 + n2;
 console.log(doTotal);
+
+
+let formatted = true;
+if(formatted){
+    console.log('This code is easy to read');
+}
+
+
+let message = "Welcome to JavaScript";
+console.log(message);
+
+
+if (window.localStorage) {
+  console.log('The local storage is supported');
+}
