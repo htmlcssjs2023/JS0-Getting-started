675fef635e6c429e8b3270e17676baf92143cc8c
mdabdulhalim <htmlcssjs2023>
2023-07-17 09:41:42 +0600

    JavaScript litle journy

---
+++ L0.js
@@ -0,0 +1,10 @@
+// two run this program type in your terminal "node L0.js"
+// Hello word prog , console.log() is used to show output in console mode.
+console.log("Hello World");
+
+
+// declare here two variable with initialization and showing total Number of two value in console mode.
+let n1 = 10;
+let n2 = 20;
+let doTotal = n1 + n2;
+console.log(doTotal);
